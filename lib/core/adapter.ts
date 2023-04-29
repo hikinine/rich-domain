5fda1c871d6e8273105c2c4cc4514ef85cb051b2
Paulo Santana <30875229+hikinine@users.noreply.github.com>
2023-04-29 19:01:36 -0300

    adapter return

--- lib/core/adapter.ts
+++ lib/core/adapter.ts
@@ -1,6 +1,7 @@
 import { BaseAggregate } from './aggregate';
+import { Either } from './result';
 
 export abstract class BaseAdapter<Aggregate extends BaseAggregate<any>, Model = any> {
-  public abstract toDomain(data: Model): Aggregate;
-  public abstract toPersistence(data: Aggregate): Model
+  public abstract toDomain(data: Model): Either<any, Aggregate>;
+  public abstract toPersistence(data: Aggregate): Either<any, Model>
 }
