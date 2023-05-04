01028dc04c61d0eefc064aa963449e3b9845f059
Paulo Santana <30875229+hikinine@users.noreply.github.com>
2023-05-04 06:47:57 -0300

    fix(pagination): include results per page

--- lib/core/pagination.ts
+++ lib/core/pagination.ts
@@ -13,12 +13,16 @@ export class Pagination<Aggregate> {
   public readonly data: Aggregate[]
   public readonly currentPage: number
   public readonly totalPages: number
+  public readonly results: number
+  public readonly totalResults: number
   public readonly orderBy?: string
 
   constructor(criteria: PaginationCriteria<any>, result: PaginationResult<any>) {
     this.currentPage = Math.floor(criteria.offset / criteria.limit) + 1
     this.totalPages = Math.ceil(result.total / criteria.limit)
-    this.data = result.data
+    this.results = result.data.length
+    this.totalResults = result.data.length
     this.orderBy = criteria?.orderBy?.join?.(" ")
+    this.data = result.data
   }
 }
