16c6acf9c5d105fe1669b9b1ac28fd698bbac14f
Paulo Santana <30875229+hikinine@users.noreply.github.com>
2023-09-26 16:51:27 -0300

    docs: apply descriptions on decorator method

--- lib/decorators/apply-rules-only-after-ends.ts
+++ lib/decorators/apply-rules-only-after-ends.ts
@@ -1,5 +1,10 @@
 import { Entity } from "../core";
 
+/**
+ * @description This decorator will ensure that the business rules will be applied only after the commit.
+ * This will help in case your method has a lot of commits, and you want to apply the rules only after the last commit.
+ * that means, if you update your entity X times in a method, the rules will be applied only after the method ends.
+ */
 export function ApplyRulesOnlyAfterCommits() {
   return function (target: Entity<any>, __: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
