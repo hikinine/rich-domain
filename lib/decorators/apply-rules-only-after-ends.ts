2c5c7b7ef4aab5db217f92cbbd082688c5ec13dd
Paulo Santana <30875229+hikinine@users.noreply.github.com>
2023-10-23 19:52:38 -0300

    feat: ensure apply-rules-only-after-commit decorator run descriptor sync

--- lib/decorators/apply-rules-only-after-ends.ts
+++ lib/decorators/apply-rules-only-after-ends.ts
@@ -5,13 +5,13 @@ import { Entity } from "../core";
  * This will help in case your method has a lot of commits, and you want to apply the rules only after the last commit.
  * that means, if you update your entity X times in a method, the rules will be applied only after the method ends.
  */
-export function ApplyRulesOnlyAfterCommits() {
+export function ApplyRulesOnlyAfterCommitsSync() {
   return function (target: Entity<any>, __: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
 
-    descriptor.value = async function (...args: any[]) {
+    descriptor.value = function (...args: any[]) {
       target['rulesIsLocked'] = true;
-      const result = await originalMethod.apply(this, args);
+      const result = originalMethod.apply(this, args);
       target['rulesIsLocked'] = false
       target.ensureBusinessRules()
       return result;
